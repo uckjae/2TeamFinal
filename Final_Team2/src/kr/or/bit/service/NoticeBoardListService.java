a09abb42d99e51dbf1bc6f6bbf4cbb706cc53e9d
kyungyeul.Choi <dnjsvltm201311681@gmail.com>
2019-11-22 18:50:50 +0900

    no message

---
+++ Final_Team2/src/kr/or/bit/service/NoticeBoardListService.java
@@ -0,0 +1,17 @@
+package kr.or.bit.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.bit.action.Action;
+import kr.or.bit.action.ActionForward;
+
+public class NoticeBoardListService implements Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
+		ActionForward forward = new ActionForward();
+		return null;
+	}
+
+}
