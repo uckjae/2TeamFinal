6aafb2818f0e4d2ed89133d23a035c8eee5d0ff1
원보연 <qhdus114@gmail.com>
2019-11-22 12:14:48 +0900

    d

--- Final_Team2/src/kr/or/bit/service/FreeBoardWriteService.java
+++ Final_Team2/src/kr/or/bit/service/FreeBoardWriteService.java
@@ -15,6 +15,7 @@ public class FreeBoardWriteService implements Action{
 		String id = (String)request.getSession().getAttribute("memberId");
 		String title = request.getParameter("title");
 		String content = request.getParameter("content");
+		System.out.println(request.getSession().getAttribute("memberId"));
 		System.out.println(id + " / " + title + " / " + content);
 		BoardDao dao = new BoardDao();
 		boolean write = dao.freeContentWrite(id, title, content);
@@ -24,11 +25,12 @@ public class FreeBoardWriteService implements Action{
 		if (write) {
 			msg = "게시글 작성 완료";
 			url = "FreeBoardDetail.do";
+			System.out.println("게시글 작성 성공");
 		} else {
 			msg = "게시글 작성 실패";
 			url = "FreeBoardWrite.do";
+			System.out.println("게시글 작성 실패");
 		}
-		System.out.println("게시글 작성 성공실패");
 		request.setAttribute("board_msg", msg);
 		request.setAttribute("board_url", url);
 
