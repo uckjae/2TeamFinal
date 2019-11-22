19de5f578750df0f7075393a52a55bd909d29587
dajeong.yoon <djyoon956@gmail.com>
2019-11-22 11:33:23 +0900

    Update QnABoardWriteDetailService.java

--- Final_Team2/src/kr/or/bit/service/QnABoardWriteDetailService.java
+++ Final_Team2/src/kr/or/bit/service/QnABoardWriteDetailService.java
@@ -10,31 +10,19 @@ import kr.or.bit.action.ActionForward;
 import kr.or.bit.dao.BoardDao;
 import kr.or.bit.dao.MemberDao;
 import kr.or.bit.dto.Member;
+import kr.or.bit.dto.QnABoard;
 
 public class QnABoardWriteDetailService implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
 		ActionForward forward = new ActionForward();
-		
-		String memberId=(String) request.getSession().getAttribute("memberId");
-		String bIdx = request.getParameter("bidx");
+
+		int bIdx = Integer.parseInt(request.getParameter("bidx")) ;
 
 		BoardDao dao = new BoardDao();
-		boolean result = dao.insertQnABoard(memberId, title, content, isPublic);
-		
-		String msg = "";
-		String url = "";
-		if (result) {
-			msg = "Q & A 작성 완료!";
-			url = "QnABoardDetail.do";
-		} else {
-			msg = "Q & A 작성 실패! 글 작성 페이지로 재 이동합니다.";
-			url = "QnABoardWrite.do";
-		}
+		QnABoard result = dao.getQnABoard(bIdx);
 
-		request.setAttribute("board_msg", msg);
-		request.setAttribute("board_url", url);
 
 		forward.setPath("/common/Redirect.jsp");
 		
