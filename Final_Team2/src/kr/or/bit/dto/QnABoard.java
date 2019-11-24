2a9ba1c62495089fef4f7c9e4b0cbae3cd9d3a14
dajeong.yoon <djyoon956@gmail.com>
2019-11-24 13:05:35 +0900

    Update QnABoard.java

--- Final_Team2/src/kr/or/bit/dto/QnABoard.java
+++ Final_Team2/src/kr/or/bit/dto/QnABoard.java
@@ -1,39 +1,54 @@
 package kr.or.bit.dto;
 
-public class QnABoard extends Board{
-	private int qIdx; //QIDX_SEQ
+import java.util.List;
+
+public class QnABoard extends Board {
+	private int qIdx; // QIDX_SEQ
 	private int bIdx;
 	private boolean isPublic;
-	
-	public QnABoard() { }
-	public QnABoard(int qIdx, int bIdx, boolean isPublic) {
+	private List<Reply> replies;
+
+	public QnABoard() {
+	}
+
+	public QnABoard(int qIdx, int bIdx, boolean isPublic, List<Reply> replies) {
 		super();
 		this.qIdx = qIdx;
 		this.bIdx = bIdx;
 		this.isPublic = isPublic;
+		this.replies = replies;
 	}
-	
+
 	public int getqIdx() {
 		return qIdx;
 	}
-	
+
 	public void setqIdx(int qIdx) {
 		this.qIdx = qIdx;
 	}
-	
+
 	public int getbIdx() {
 		return bIdx;
 	}
-	
+
 	public void setbIdx(int bIdx) {
 		this.bIdx = bIdx;
 	}
-	
+
 	public boolean isPublic() {
 		return isPublic;
 	}
-	
+
 	public void setPublic(boolean isPublic) {
 		this.isPublic = isPublic;
 	}
+
+	public List<Reply> getReplies() {
+		return replies;
+	}
+
+	public void setReplies(List<Reply> replies) {
+		this.replies = replies;
+	}
+
 }
