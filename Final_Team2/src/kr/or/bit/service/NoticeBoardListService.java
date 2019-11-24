a15267a236f1e9b4b8ae19b6e3279c4857f806f1
kyungyeul.Choi <dnjsvltm201311681@gmail.com>
2019-11-24 11:17:10 +0900

    no message

--- Final_Team2/src/kr/or/bit/service/NoticeBoardListService.java
+++ Final_Team2/src/kr/or/bit/service/NoticeBoardListService.java
@@ -1,20 +1,27 @@
 package kr.or.bit.service;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kr.or.bit.action.Action;
 import kr.or.bit.action.ActionForward;
+import kr.or.bit.dao.BoardDao;
+import kr.or.bit.dto.NoticeBoard;
 
 public class NoticeBoardListService implements Action{
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
 		ActionForward forward = new ActionForward();
-		
-		
-		
+
+		BoardDao dao = new BoardDao();
+		List<NoticeBoard> nboard = dao.noticeList();
+		request.setAttribute("noboard", nboard);
+		forward.setPath("/WEB-INF/views/board/notice/Main.jsp");
+
 		return forward;
 	}
-
 }
+
