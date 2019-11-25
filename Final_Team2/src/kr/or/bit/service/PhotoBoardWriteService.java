79cbf09f83a4599c3417d32947246604927ceb3a
ILGYEOM <chdl1229@gmail.com>
2019-11-25 17:35:15 +0900

    수정 작업중

--- Final_Team2/src/kr/or/bit/service/PhotoBoardWriteService.java
+++ Final_Team2/src/kr/or/bit/service/PhotoBoardWriteService.java
@@ -7,6 +7,7 @@ import kr.or.bit.action.Action;
 import kr.or.bit.action.ActionForward;
 import kr.or.bit.dao.BoardDao;
 import kr.or.bit.dto.Board;
+import kr.or.bit.dto.Photo;
 
 public class PhotoBoardWriteService implements Action{
 	@Override
@@ -14,18 +15,19 @@ public class PhotoBoardWriteService implements Action{
 		ActionForward forward = new ActionForward();
 		
 		Board photowrite = null;
-		
+		Photo photo = null;
 		String cmd = request.getParameter("cmd");
 		
 		if(cmd.equals("write")) {
 			photowrite = new Board(); 
-		//	photowrite.setbIdx(-1);
+			photowrite.setbIdx(-1);
 		}else if(cmd.equals("edit")) {
 			int bIdx = Integer.parseInt(request.getParameter("bIdx"));
 			BoardDao dao = new BoardDao();
+			photo = dao.boardDetailPhoto(bIdx).get(0);
 			photowrite = dao.getBoardByBIdx(bIdx);
 		}
-		
+		request.setAttribute("photo", photo);
 		request.setAttribute("photowrite", photowrite);
 		forward.setPath("/WEB-INF/views/board/photo/Write.jsp");
 		return forward;
