b498f7e8dbaa94c3b5b1fcfe47c630bc1a809535
dajeong.yoon <djyoon956@gmail.com>
2019-11-30 16:14:56 +0900

    Update checkReadBoad.js

--- Final_Team2/WebContent/js/checkReadBoad.js
+++ Final_Team2/WebContent/js/checkReadBoad.js
@@ -1,11 +1,8 @@
 
 function checkReadBoad(result, bIdx, href){
-	console.log("result");
-	console.log("result" + result);
 	if(result){
 		location.href=href+"?bIdx="+bIdx;
 	}else{
-		alert("로그인하세요");
-		location.href="Login.do";
+		warningAlert("로그인하세요");
 	}
 }
