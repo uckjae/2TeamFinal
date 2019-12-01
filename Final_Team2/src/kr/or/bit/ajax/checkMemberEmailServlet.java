9756929d558c192fce42d1d5cfb4543a88f9f7c9
dajeong.yoon <djyoon956@gmail.com>
2019-12-01 16:06:30 +0900

    가입 오류 확인

---
+++ Final_Team2/src/kr/or/bit/ajax/checkMemberEmailServlet.java
@@ -0,0 +1,48 @@
+package kr.or.bit.ajax;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.bit.dao.MemberDao;
+
+@WebServlet("/CheckMemberEmail")
+public class checkMemberEmailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public checkMemberEmailServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+
+		PrintWriter out = response.getWriter();
+
+		String email = request.getParameter("email");
+
+		MemberDao dao = new MemberDao();
+		boolean result = dao.isUseMemberEmail(email);
+
+		out.print(result);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+}
